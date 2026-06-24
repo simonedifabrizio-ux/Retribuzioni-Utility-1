9147243b702ef25b102726351acd42d4785ea026
Simone Di Fabrizio <simone.difabrizo.sdf@gmail.com>
2026-06-24 08:34:22 +0200

    Ottimizza il layout verticale

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retribuzioni-utility-v1';
+const CACHE = 'retribuzioni-utility-v2';
 const APP_FILES = [
   './',
   './index.html',
@@ -26,6 +26,20 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match(event.request).then(cached =>
+        cached || caches.match(new URL('./index.html', self.registration.scope).href)
+      ))
+    );
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
       const copy = response.clone();
